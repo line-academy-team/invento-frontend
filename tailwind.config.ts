ddcbf8590868f784bb62800a1794282f7a5c05ec
ehdgus02 <ehdgus02kk@naver.com>
2026-07-22 15:11:48 +0900

    tailwind 팔레트, 기본 색상 globals.css 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import { Config } from "tailwindcss";
 
+type CustomConfig = Config & {
+    safelist?: Array<string | { pattern: RegExp; variants?: string[] }>;
+};
+
 export default {
     darkMode: "class",
     content: [
@@ -18,8 +22,48 @@ export default {
                 pretendard: ["Pretendard-Regular", "sans-serif"],
             },
             colors: {
+                background: {
+                    default: "var(--background)",
+                    paper: "var(--surface)",
+                },
+                text: {
+                    default: "var(--text)",
+                    secondary: "var(--text-secondary)",
+                    light: "var(--text-light)",
+                },
+                divider: "var(--border)",
 
+                primary: {
+                    main: "var(--primary)",
+                    hover: "var(--hover)",
+                    active: "var(--active)",
+                    light: "var(--light)",
+                    contrast: "var(--text-light)",
+                },
+                secondary: {
+                    main: "var(--secondary)",
+                    hover: "var(--secondary-hover)",
+                    contrast: "var(--text-light)",
+                },
+                error: {
+                    main: "var(--error)",
+                    contrast: "var(--text-light)",
+                },
+                success: {
+                    main: "var(--success)",
+                    contrast: "var(--text-light)",
+                },
+                warning: {
+                    main: "var(--warning)",
+                    contrast: "var(--text-light)",
+                },
             },
         },
     },
-} satisfies Config;
+    safelist: [
+        {
+            pattern:
+                /(bg|text|border)-(primary|secondary|error|success|warning)-(main|hover|active|light|contrast)/,
+        },
+    ],
+} satisfies CustomConfig;
