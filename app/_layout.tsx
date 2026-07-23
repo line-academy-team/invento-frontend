d491c6b36e1dd0fd0ffb697b4fcd9f7311ddd88d
hyw0206 <hyw0206s@naver.com>
2026-07-23 11:07:15 +0900

    nativewind 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,38 @@
+import "../styles/global.css"
+
+import * as SplashScreen from "expo-splash-screen"
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { Slot, SplashScreen} from "expo-router"
-import {useFonts} from "expo-font";
-import {useEffect} from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router"
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { useThemeStore } from "@/stores/theme/useThemeStore";
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync().then(() => {});
 
 export default function RootLayout() {
 
+    const { theme } = useThemeStore();
+
     const [loaded, error] = useFonts({
-        'Pretendard-Regular': require('pretendard/dist/web/static/woff2/Pretendard-Regular.woff2'),
-        'Pretendard-SemiBold': require('pretendard/dist/web/static/woff2/Pretendard-SemiBold.woff2'),
-        'Pretendard-Bold': require('pretendard/dist/web/static/woff2/Pretendard-Bold.woff2'),
+        "Pretendard-Thin": require("@/assets/fonts/Pretendard-Thin.otf"),
+        "Pretendard-ExtraLight": require("@/assets/fonts/Pretendard-ExtraLight.otf"),
+        "Pretendard-Light": require("@/assets/fonts/Pretendard-Light.otf"),
+        "Pretendard-Regular": require("@/assets/fonts/Pretendard-Regular.otf"),
+        "Pretendard-Medium": require("@/assets/fonts/Pretendard-Medium.otf"),
+        "Pretendard-SemiBold": require("@/assets/fonts/Pretendard-SemiBold.otf"),
+        "Pretendard-Bold": require("@/assets/fonts/Pretendard-Bold.otf"),
+        "Pretendard-ExtraBold": require("@/assets/fonts/Pretendard-ExtraBold.otf"),
+        "Pretendard-Black": require("@/assets/fonts/Pretendard-Black.otf"),
     });
 
     useEffect(() => {
         if (loaded || error) {
-            SplashScreen.hideAsync();
+            SplashScreen.hideAsync().then(() => {});
         }
     }, [loaded, error])
 
@@ -30,10 +42,12 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-        <StatusBar />
-      <SafeAreaView style={{ flex: 1 }}>
-        <Slot />
-      </SafeAreaView>
+        <StatusBar style={
+            theme === "dark" ? "light" : "dark"
+        }/>
+        <SafeAreaView className={"flex-1"}>
+             <Slot />
+        </SafeAreaView>
     </SafeAreaProvider>
   );
 }
