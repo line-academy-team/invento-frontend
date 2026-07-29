04881df6a104830e5be0d5d889c976c24eee7636
molang404 <art104233@naver.com>
2026-07-29 11:09:03 +0900

    equipment-unit 추가

--- api/manager/managerEquipmentApi.ts
+++ api/manager/managerEquipmentApi.ts
@@ -3,6 +3,11 @@ import {
     UpdateEquipmentInputType,
 } from "@/schemas/manager/managerEquipmentSchema";
 import axiosInstance from "@/api/axiosInstance";
+import {
+    CreateEquipmentUnitInputType,
+    UpdateEquipmentUnitInputType,
+} from "@/schemas/manager/managerEquipmentUnitSchema";
+import { EquipmentUnit } from "@/types/equipmentUnit";
 
 const createEquipment = async (input: CreateEquipmentInputType) => {
     const response = await axiosInstance.post("/equipment/create", input);
@@ -18,8 +23,25 @@ const deleteEquipment = async (equipmentId: number) => {
     await axiosInstance.delete(`/equipment/${equipmentId}`);
 };
 
+const createUnit = async (input: CreateEquipmentUnitInputType): Promise<EquipmentUnit> => {
+    const response = await axiosInstance.post("/equipment-unit", input);
+    return response.data.data;
+}
+
+const updateUnit = async (unitId: number, input: UpdateEquipmentUnitInputType): Promise<EquipmentUnit> => {
+    const response = await axiosInstance.patch(`/equipment-unit/${unitId}`, input);
+    return response.data.data;
+}
+
+const deleteUnit = async (unitId: number)=> {
+    await axiosInstance.delete(`/equipment-unit/${unitId}`);
+};
+
 export default {
     createEquipment,
     updateEquipment,
     deleteEquipment,
+    createUnit,
+    updateUnit,
+    deleteUnit,
 };
